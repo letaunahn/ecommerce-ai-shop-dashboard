f44dd59d4c7cd2375b2e56f45120421fb6d8d92a
letaunahn <letuananh28072002@gmail.com>
2025-11-15 01:56:10 +0700

    Change backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
